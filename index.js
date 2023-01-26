ce291f6f583dfb02b19b88c4ded6f01f1ae9b670
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-01-26 16:47:39 +0300

    Events section styled

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ function gotoDate() {
 
     if (dateArr.length === 2) {
         if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
-            month === dateArr[0] - 1; year = dateArr[1];
+            month === dateArr[0] - 1; 
+            year = dateArr[1];
             initCalendar();
             return;
         }
