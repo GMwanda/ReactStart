8977ab66a6d6c5ca0fa00505ad2b23e9cfc6e938
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-01-26 09:48:32 +0300

    Adding of button listenners

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const calendar = document.querySelector(".calendar"),
     date = document.querySelector(".date"),
     daysContainer = document.querySelector(".days"),
     prev = document.querySelector(".bx-caret-left-circle"),
-    next = document.querySelector(".bx-caret-right-circle");
+    next = document.querySelector(".bx-caret-right-circle"),
+    todayyBtn = document.querySelector(".today-btn"),
+    gotobtn = document.querySelector(".goto-btn "),
+    dateInput = document.querySelector(".date-input");
+
 
 let today = new Date();
 let activeDay;
@@ -74,6 +78,51 @@ function nextMonth() {
     }
     initCalendar();
 }
-
+// buttons
 prev.addEventListener("click", prevMonth);
 next.addEventListener("click", nextMonth);
+
+// GOES TO TODAY
+todayyBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("keyup", () => {
+    // ALLOW ONLY NUMBERS REMOVE ANYTHING ELSE
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, '');
+
+    if (dateInput.value.length === 2) {
+        // ADD SLASH IF 2 NUMBERS ENTERED
+        dateInput.value += "/";
+    }
+    if (dateInput.value.length > 7) {
+        // DONT ALLOW MORE THAN 7 CHARACTERS
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+    // IF backspace is pressed
+    if (e.inputType = "deleteContentBackward") {
+        if (dateInput.value.length === 3) {
+            dateInput.value = dateInput.value.slice(0, 2);
+        }
+    }
+
+});
+
+gotobtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month === dateArr[0] - 1; year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+
+    alert("Invalid date");
+}
